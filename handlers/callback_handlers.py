cee6b85df451ac0c3e57486adc562029b30f6ba3
Dmitry <dr_storn@mail.ru>
2024-01-12 08:28:39 +0300

    VPS

--- handlers/callback_handlers.py
+++ handlers/callback_handlers.py
@@ -2,10 +2,10 @@ from aiogram import Router, F
 from aiogram.types import CallbackQuery
 
 
-
-
 router = Router()
+
 
 @router.callback_query(F.data.startswith("LPF"))
 async def lpf_filial(call: CallbackQuery):
-    await call.message.answer(call.data)
+    await call.message.answer("Липецкий филиал:")
+    await call.answer()
